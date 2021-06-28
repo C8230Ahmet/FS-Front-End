18d8b9420ef46ee39805cff758212210578bc101
C8230Ahmet <ahmtanlargu@gmail.com>
2021-06-28 22:53:25 +0300

    a

---
+++ HTML/dersler/js05.js
@@ -0,0 +1,32 @@
+// 1. WHILE LOOPS
+/*
+let i = 0,
+  sum = 0;
+while (i < 6) {
+  sum += i;
+  console.log(i);
+  i++;
+}
+console.log(`Total is : ${sum}`);
+*/
+// 2. DO WHILE
+/*
+let i = 3,
+  sum = 0;
+do {
+  sum += i;
+  i++;
+} while (i < 5);
+console.log(sum);
+*/
+// 3. For Loops
+/*
+let i = 4;
+for (; i < 8; ) {
+  console.log("i", i);
+  i++;
+}
+*/
+for (i = 4; i < 8; i++) {
+  console.log("i", i);
+}
